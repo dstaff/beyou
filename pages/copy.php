34b70ef9f788fd16b53f0996de5b8d0e7ae12248
Danis staff <dstaff@bladex.com>
2024-05-07 16:22:18 -0500

    minor improvements

--- pages/copy.php
+++ pages/copy.php
@@ -1,3 +1,3 @@
 <div class="py-6 px-6 text-center">
-    <p class="mb-0 fs-4">Design and Developed by <a href="https://thedstaff.com" target="_blank" class="pe-1 text-primary text-decoration-underline">DStaff</a></p>
+    <p class="mb-0 fs-4">Design and Developed by <a href="https://thedstaff.com" target="_blank" class="pe-1 text-primary text-decoration-underline">DStaff</a> - <?php echo date('Y');?></p>
 </div>
