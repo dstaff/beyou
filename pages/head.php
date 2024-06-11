015055858fdb595ac019da21fafbb9141767f339
danis.staff <danis.staff@gmail.com>
2024-06-11 17:01:56 +0000

    test

--- pages/head.php
+++ pages/head.php
@@ -11,6 +11,6 @@ if(!isset($_SESSION["id"]))
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?php echo empty($title) ? "BeYou" : $title; ?></title>
-  <link rel="shortcut icon" type="image/png" href="assets/images/logos/favicon.png" />
-  <link rel="stylesheet" href="assets/css/styles.min.css" />
+  <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png" />
+  <link rel="stylesheet" href="../assets/css/styles.min.css" />
 </head>
